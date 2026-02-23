3f4909c073daeefe3f907fcb2a2d375903164f1d
Jusales <jusales3270@gmail.com>
2026-02-23 14:03:49 -0300

    corrige anon key completa do supabase

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
 
 const SUPABASE_CONFIG = {
   URL: 'https://piiccitnhcjhpadoqbk.supabase.co',
-  ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpaWNjaXRuaGNqaHBhZG9xYmsiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTcwNjY0ODAwMCwiZXhwIjoyMDIyMjI0MDAwfQ.qRk26nQ-ffhDMMbxvAuF6A_BZEid9ef'
+  ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpaWNjaXRuaGNqaHBhYWRvcWJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4NjAzODUsImV4cCI6MjA4NzQzNjM4NX0.GyvASJ05PWjQepQWVaYGFxiKXHAYFx5Gn2IKvk674PQ'
 };
 
 // Exporta para uso nos módulos
